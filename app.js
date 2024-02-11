5ccc3e49c7cfe9814bb42024e3317f7989a0caba
mike030188 <halol.imnida2023@gmail.com>
2024-02-11 19:00:52 +0500

    feat: MongoDB connection logic

---
+++ app.js
@@ -0,0 +1,62 @@
+console.log("Web Serverni boshlash");
+const express = require('express'); // EXTERNAL package
+const app = express(); // application variable
+const fs = require('fs'); // CORE package
+
+
+// MongoDB chaqiriw...
+const db = require("./server").db(); // shu "db" => orqali biz MongoDB da ma`lumotlarni uqiw, edit, delete qilishimiz mumkin
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERRORR", err);
+    } else {
+        user = JSON.parse(data) // Converts a JavaScript Object Notation (JSON) string into an object.
+    }
+});
+
+
+// *** Express Server qurish bosqichlari ***
+
+// 1 Kirish code
+app.use(express.static("public")); // bu clientga faqat public folderlarni korsatish
+app.use(express.json()); // bu json formatni objectga aylantirib berish
+app.use(express.urlencoded({extended: true})); // HTML forum dan kelgan habarlarni express qabul qilishi un, aks holda "ignore qiladi"
+
+// 2 Session code (in this project session is not applied)
+
+
+// 3 View code. 
+// BSSR (BackEnd Server Side Rendering => BackEndda view (FrontEnd) yasash). Bu "Traditional" usul xisoblanadi! => EJS dan foydalanamiz
+app.set("views", "views"); // bu yerda "views"-> folder nomi
+app.set("view engine", "ejs");
+
+// 4 Routing code (Web sahifani o`rnatilgan portda korinishini ta`minlaydi)
+// app.get("/hello", function (req, res) {
+//     res.end(`<h1 style="background: red">HELLO WORLD!</h1>`);
+// });
+// app.get("/gift", function (req, res) {
+//     res.end(`<h1 style="background: green">Siz sovgalar sahifasidasiz</h1>`);
+// });
+
+// bu yerda "post" => O`zi bn ma`lumotni olib kelib Database ga o`sha ma`lumotni yozish un kk
+app.post("/create-item", (req, res) => {
+    // TODO: code with db here
+
+    // console.log(req.body);
+    // console.log(req);  => 3 qism korinadi
+    // res.json({test: "success"});
+});
+
+app.get('/author', (req, res) => {
+    res.render("author", { user: user })
+});
+
+// bu yerda "get" => Database dan ma`lumotni olish (o`qish) un kk
+app.get("/", function (req, res) {
+    res.render("reja");
+});
+
+module.exports = app;
+
--- server.js
+++ server.js
@@ -1,65 +1,33 @@
-console.log("Web Serverni boshlash");
-const express = require('express'); // EXTERNAL package
-const app = express(); // application variable
 const http = require('http'); // CORE package
-const fs = require('fs'); // CORE package
-
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if (err) {
-        console.log("ERRORR", err);
-    } else {
-        user = JSON.parse(data) // Converts a JavaScript Object Notation (JSON) string into an object.
-    }
-});
+const mongodb = require('mongodb');
 
 
-// *** Express Server qurish bosqichlari ***
+// MongoDB connect (1-step) ... 2-step is connect to Server
+let db;
+const connectionString =
+  "mongodb+srv://mike030188:zVniEljrW3IXEYwI@cluster0.lpbal5g.mongodb.net/Reja";
 
-// 1 Kirish code
-app.use(express.static("public")); // bu clientga faqat public folderlarni korsatish
-app.use(express.json()); // bu json formatni objectga aylantirib berish
-app.use(express.urlencoded({extended: true})); // HTML forum dan kelgan habarlarni express qabul qilishi un, aks holda "ignore qiladi"
+mongodb.connect(connectionString, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }, 
+    (err, client) => {
+        if (err) console.log("ERROR on connection MongoDB");
+        else {
+            console.log("MongoDB connection succeed");
+            // console.log(client); "client" ni biz kelejakda juda kop iwlatamiz shuning un uni export qivolamiz
+            module.exports = client;
 
-// 2 Session code (in this project session is not applied)
+            const app = require('./app'); 
+            // Server connect (2-step)
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function () {
+                console.log(
+                    `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+                );
+            });
+        }
+    })
 
-
-// 3 View code. 
-// BSSR (BackEnd Server Side Rendering => BackEndda view (FrontEnd) yasash). Bu "Traditional" usul xisoblanadi! => EJS dan foydalanamiz
-app.set("views", "views"); // bu yerda "views"-> folder nomi
-app.set("view engine", "ejs");
-
-// 4 Routing code (Web sahifani o`rnatilgan portda korinishini ta`minlaydi)
-// app.get("/hello", function (req, res) {
-//     res.end(`<h1 style="background: red">HELLO WORLD!</h1>`);
-// });
-// app.get("/gift", function (req, res) {
-//     res.end(`<h1 style="background: green">Siz sovgalar sahifasidasiz</h1>`);
-// });
-
-// bu yerda "post" => O`zi bn ma`lumotni olib kelib Database ga o`sha ma`lumotni yozish un kk
-app.post("/create-item", (req, res) => {
-    // TODO: code with db here
-
-    // console.log(req.body);
-    // console.log(req);  => 3 qism korinadi
-    // res.json({test: "success"});
-});
-
-app.get('/author', (req, res) => {
-    res.render("author", { user: user })
-});
-
-// bu yerda "get" => Database dan ma`lumotni olish (o`qish) un kk
-app.get("/", function (req, res) {
-    res.render("reja");
-});
-
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-    console.log(`
-        The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
-    );
-});
