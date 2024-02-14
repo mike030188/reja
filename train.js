58f42df1136349e298de79b49cf171b67d9823af
mike030188 <halol.imnida2023@gmail.com>
2024-02-14 14:37:37 +0500

    feat: Reja loyihamizni yakuniy bosqichi

--- train.js
+++ train.js
@@ -1,6 +1,46 @@
 console.log("TASK ishga tuwdi");
 
+// *** D - TASK ***
+// Shunday class tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin,
+// hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul.
+// Har bir method ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return
+// hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! 
+// shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return
+// hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
 
+const moment = require("moment");
+const time = moment().format("HH:mm")
+class shop {
+    kebab;
+    lavash;
+    cola;
+
+    constructor(kebab, lavash, cola) {
+        this.kebab = kebab;
+        this.lavash = lavash;
+        this.cola = cola;
+    }
+    
+    qoldiq(){
+        console.log(`Hozir ${time}da ${this.kebab}ta kebab, ${this.lavash}ta lavash va ${this.cola}ta cola mavjud!`);
+    }
+    sotish(kebab){
+        this.kebab -= kebab;
+        console.log(`Biz ${kebab}ta kebab sotdik va hozir ${time}da ${this.kebab}ta kebab, ${this.lavash}ta lavash va ${this.cola}ta cola qoldi `);
+    }
+    qabul(cola){
+        this.cola += cola;
+        console.log(`biz ${cola}ta cola qabul qildik va Hozir ${time}da ${this.kebab}ta kebab, ${this.lavash}ta lavash va ${this.cola}ta cola boldi `);
+    }
+}
+
+ 
+
+ const myShop = new shop(4,5,2);
+ myShop.qoldiq();
+ myShop.sotish(3);
+ myShop.qabul(4);
 
 
 
